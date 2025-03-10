25e45708c7b533e78e913b21c330b40f3c600be5
Austin <declerckaustin@gmail.com>
2025-03-10 17:40:53 -0500

    nodemon server setup and environment fix

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000;
 
 mongoose
-    .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(process.env.MONGODB_URI)
     .then(() => console.log('MongoDB connected'))
     .catch((err) => console.error('MongoDB connection error:', err));
 
